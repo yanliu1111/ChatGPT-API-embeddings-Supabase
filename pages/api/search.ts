e55e6e74af5866ce7e1692ed0922afd26c610b3c
yanliu1111 <liuyan0504@gmail.com>
2023-04-11 15:23:40 -0600

    feat: streaming func setting in search.ts

---
+++ pages/api/search.ts
@@ -0,0 +1,28 @@
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  try {
+    const { query } = (await req.json()) as { query: string };
+
+    const response = await fetch(`https://api.openai.com/v1/embeddings`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`,
+      },
+      body: JSON.stringify({
+        model: "text-embedding-ada-002",
+        input: query,
+      }),
+    });
+
+    const json = await response.json();
+    const embedding = json.data[0].embedding;
+  } catch (e) {
+    return new Response("Error", { status: 500 });
+  }
+};
+
+export default handler;
