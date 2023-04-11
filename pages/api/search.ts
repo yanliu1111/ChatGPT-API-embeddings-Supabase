f30dfb2f346dc9b9af110a9251e67b7475725f35
yanliu1111 <liuyan0504@gmail.com>
2023-04-11 15:55:04 -0600

    feat: API handler fetch db chunk data

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,3 +1,5 @@
+import { supabaseAdmin } from "@/utils";
+
 export const config = {
   runtime: "edge",
 };
@@ -20,6 +22,20 @@ const handler = async (req: Request): Promise<Response> => {
 
     const json = await response.json();
     const embedding = json.data[0].embedding;
+
+    const { data: chunks, error } = await supabaseAdmin.rpc(
+      "paul_graham_search",
+      {
+        query_embedding: embedding,
+        similarity_threshold: 0.5,
+        match_count: 5,
+      }
+    );
+    if (error) {
+      console.log(error);
+      return new Response("Error", { status: 500 });
+    }
+    return new Response(JSON.stringify(chunks), { status: 200 });
   } catch (e) {
     return new Response("Error", { status: 500 });
   }
